12622e3d021ff65487188cfe1ca9fb8a504e9ca4
Diego Testi <diego.testi@loviz.com.vc>
2023-11-13 08:00:47 -0300

    update practice

---
+++ practice/public/bandersnatch/src/videoPlayer.js
@@ -0,0 +1,104 @@
+class VideoMediaPlayer {
+  constructor({ manifestJSON, network, videoComponent }) {
+    this.manifestJSON = manifestJSON;
+    this.network = network;
+    this.videoComponent = videoComponent
+
+    this.videoElement = null;
+    this.sourceBuffer = null;
+    this.activeItem = {}
+    this.selected = {};
+    this.videoDuration = 0;
+  }
+
+  initializeCodec() {
+    this.videoElement = document.getElementById("vid");
+    const mediaSourceSupported = !!window.MediaSource;
+    if (!mediaSourceSupported) {
+      alert("Seu browser ou sistema não tem suporte a MSE!");
+      return;
+    }
+    
+    const codecSupported = MediaSource.isTypeSupported(this.manifestJSON.codec);
+    if (!codecSupported) {
+      alert(`seu browser não suporta o codec: ${this.manifestJSON.codec}`);
+    }
+    const mediaSource = new MediaSource();
+    this.videoElement.src = URL.createObjectURL(mediaSource);
+
+    mediaSource.addEventListener(
+      "sourceopen",
+      this.sourceOpenWrapper(mediaSource)
+    );
+  }
+  sourceOpenWrapper(mediaSource) {
+    return async (_) => {
+      this.sourceBuffer = mediaSource.addSourceBuffer(this.manifestJSON.codec);
+      const selected = this.selected = this.manifestJSON.intro;
+      // evita rodar como 'live'
+      mediaSource.duration = this.videoDuration;
+      await this.fileDownload(selected.url);
+      setInterval(this.waitForQuestions.bind(this), 200)
+    };
+  }
+
+  waitForQuestions() {
+    const currentTime = parseInt(this.videoElement.currentTime)
+    const option = this.selected.at === currentTime
+    if(!option) return
+
+      // evita que o modal seja aberto 2x no mesmo segundo
+    if(this.activeItem.url === this.selected.url) return
+    this.videoComponent.configureModal(this.selected.options)
+    this.activeItem = this.selected
+  }
+
+  async nextChunk(data) {
+    const key = data.toLowerCase()
+    const selected = this.manifestJSON[key]
+    this.selected = {
+        ...selected,
+        // ajusta o tempo que o modal aparece, baseado no tempo corrente
+        at: parseInt(this.videoElement.currentTime + selected.at)
+    }
+    // deixa o restante do vídeo rodar
+    this.videoElement.play()
+    await this.fileDownload(selected.url)
+  }
+
+  async fileDownload(url) {
+    const prepareUrl = {
+      url,
+      fileResolution: 360,
+      fileResolutionTag: this.manifestJSON.fileResolutionTag,
+      hostTag: this.manifestJSON.hostTag,
+    };
+
+    const finalUrl = this.network.parseManifestUrl(prepareUrl);
+    this.setVideoPlayerDuration(finalUrl);
+    const data = await this.network.fetchFile(finalUrl);
+    return this.processBufferSegments(data);
+  }
+
+  setVideoPlayerDuration(finalUrl) {
+    const bars = finalUrl.split("/");
+    const [name, videoDuration] = bars[bars.length - 1].split("-");
+    this.videoDuration += parseFloat(videoDuration);
+  }
+  async processBufferSegments(allSegments) {
+    const sourceBuffer = this.sourceBuffer;
+    sourceBuffer.appendBuffer(allSegments);
+
+    return new Promise((resolve, reject) => {
+      const updateEnd = (_) => {
+        sourceBuffer.removeEventListener("updateend", updateEnd);
+        sourceBuffer.timestampOffset = this.videoDuration;
+
+        return resolve();
+      };
+
+      sourceBuffer.addEventListener("updateend", updateEnd);
+      sourceBuffer.addEventListener("error", reject);
+    });
+  }
+}
