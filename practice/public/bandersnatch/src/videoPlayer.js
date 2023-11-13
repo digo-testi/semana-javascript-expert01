b6932593ea872fb56a024fc10f124c04f6b8928b
Diego Testi <diego.testi@loviz.com.vc>
2023-11-13 08:52:52 -0300

    complete trainning

--- practice/public/bandersnatch/src/videoPlayer.js
+++ practice/public/bandersnatch/src/videoPlayer.js
@@ -2,13 +2,14 @@ class VideoMediaPlayer {
   constructor({ manifestJSON, network, videoComponent }) {
     this.manifestJSON = manifestJSON;
     this.network = network;
-    this.videoComponent = videoComponent
+    this.videoComponent = videoComponent;
 
     this.videoElement = null;
     this.sourceBuffer = null;
-    this.activeItem = {}
+    this.activeItem = {};
     this.selected = {};
     this.videoDuration = 0;
+
   }
 
   initializeCodec() {
@@ -18,7 +19,7 @@ class VideoMediaPlayer {
       alert("Seu browser ou sistema não tem suporte a MSE!");
       return;
     }
-    
+
     const codecSupported = MediaSource.isTypeSupported(this.manifestJSON.codec);
     if (!codecSupported) {
       alert(`seu browser não suporta o codec: ${this.manifestJSON.codec}`);
@@ -34,42 +35,64 @@ class VideoMediaPlayer {
   sourceOpenWrapper(mediaSource) {
     return async (_) => {
       this.sourceBuffer = mediaSource.addSourceBuffer(this.manifestJSON.codec);
-      const selected = this.selected = this.manifestJSON.intro;
+      const selected = (this.selected = this.manifestJSON.intro);
       // evita rodar como 'live'
       mediaSource.duration = this.videoDuration;
       await this.fileDownload(selected.url);
-      setInterval(this.waitForQuestions.bind(this), 200)
+      setInterval(this.waitForQuestions.bind(this), 200);
     };
   }
 
   waitForQuestions() {
-    const currentTime = parseInt(this.videoElement.currentTime)
-    const option = this.selected.at === currentTime
-    if(!option) return
+    const currentTime = parseInt(this.videoElement.currentTime);
+    const option = this.selected.at === currentTime;
+    if (!option) return;
 
-      // evita que o modal seja aberto 2x no mesmo segundo
-    if(this.activeItem.url === this.selected.url) return
-    this.videoComponent.configureModal(this.selected.options)
-    this.activeItem = this.selected
+    // evita que o modal seja aberto 2x no mesmo segundo
+    if (this.activeItem.url === this.selected.url) return;
+    this.videoComponent.configureModal(this.selected.options);
+    this.activeItem = this.selected;
+  }
+
+  async currentFileResolution() {
+    const LOWEST_RESOLUTION = 144;
+    const preapreUrl = {
+      url: this.manifestJSON.finalizar.url,
+      fileResolution: LOWEST_RESOLUTION,
+      fileResolutionTag: this.manifestJSON.fileResolutionTag,
+      hostTag: this.manifestJSON.hostTag,
+    };
+    const url = this.network.parseManifestUrl(preapreUrl);
+    return this.network.getProperResolution(url);
   }
 
   async nextChunk(data) {
-    const key = data.toLowerCase()
-    const selected = this.manifestJSON[key]
+    const key = data.toLowerCase();
+    const selected = this.manifestJSON[key];
     this.selected = {
-        ...selected,
-        // ajusta o tempo que o modal aparece, baseado no tempo corrente
-        at: parseInt(this.videoElement.currentTime + selected.at)
-    }
+      ...selected,
+      // ajusta o tempo que o modal aparece, baseado no tempo corrente
+      at: parseInt(this.videoElement.currentTime + selected.at),
+    };
+    this.manageLag(selected)
     // deixa o restante do vídeo rodar
-    this.videoElement.play()
-    await this.fileDownload(selected.url)
+    this.videoElement.play();
+    await this.fileDownload(selected.url);
+  }
+  manageLag(selected){
+    if(!!~this.selections.indexOf(selected.url)){
+      selected.at += 5
+      return
+    }
+    this.selections.push(selected.url)
   }
 
   async fileDownload(url) {
+    const fileResolution = await this.currentFileResolution();
+    console.log("resolução", fileResolution);
     const prepareUrl = {
       url,
-      fileResolution: 360,
+      fileResolution: fileResolution,
       fileResolutionTag: this.manifestJSON.fileResolutionTag,
       hostTag: this.manifestJSON.hostTag,
     };
